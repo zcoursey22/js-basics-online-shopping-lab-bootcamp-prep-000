ca1d75685f7250189d9eef6a610fb5e587189ef8
Zachary Coursey <zcoursey22@gmail.com>
2018-02-02 02:44:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -63,5 +63,8 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   if (!cardNumber) {
     console.log("Sorry, we don't have a credit card on file for you.")
+  } else {
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
+    cart = [];
   }
 }
