a7e5c9e787189afbd15fc13987d4fe058ffac291
Zachary Coursey <zcoursey22@gmail.com>
2018-02-02 02:16:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -42,7 +42,11 @@ function viewCart() {
 }
 
 function total() {
-  // write your code here
+  var total = 0;
+  for (var i = 0; i < cart.length; i++) {
+    total += 
+  }
+  return total;
 }
 
 function removeFromCart(item) {
