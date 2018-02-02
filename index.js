b090e4105943621ab62e82d4bd98e5757908fe71
Zachary Coursey <zcoursey22@gmail.com>
2018-02-02 02:24:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ function viewCart() {
 function total() {
   var total = 0;
   for (var i = 0; i < cart.length; i++) {
-    total += Object.keys(cart[i])
+    total += cart[i][Object.keys(cart[i]);
   }
   return total;
 }
