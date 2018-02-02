0f618f40a53adf7ecb21c9d87c1dc9dc715ffec1
Zachary Coursey <zcoursey22@gmail.com>
2018-02-02 02:35:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ function total() {
 
 function removeFromCart(item) {
   for (var i = 0; i < cart.length; i++) {
-    if (item === Object.keys(cart[i])) {
+    if (item === Object.keys(cart[i]).toString()) {
       cart.splice(i, 1);
       return cart;
     }
